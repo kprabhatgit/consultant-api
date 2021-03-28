260c7cb34440d9406ecec6491e5c02733d72a79c
kprabhatgit <k.prabhat28@gmail.com>
2021-03-28 20:41:37 +0530

    first commit

---
+++ src/main/java/com/prabhat/consultantapi/ConsultantApiApplication.java
@@ -0,0 +1,16 @@
+package com.prabhat.consultantapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class ConsultantApiApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(ConsultantApiApplication.class, args);
+	}
+
+}
